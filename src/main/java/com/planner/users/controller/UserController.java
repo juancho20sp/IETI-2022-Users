e4282f8e34dba80346b1b1a330a83fe08f9ad02e
Juan David Murillo <juancho20sp@gmail.com>
2022-08-26 14:43:39 -0500

    UserController added

---
+++ src/main/java/com/planner/users/controller/UserController.java
@@ -0,0 +1,75 @@
+package com.planner.users.controller;
+
+import com.planner.users.dto.UserDto;
+import com.planner.users.entities.User;
+import com.planner.users.service.UserService;
+import com.planner.users.service.exceptions.UserServiceException;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(@Autowired UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAll() {
+        return new ResponseEntity<>(userService.getAll(), HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping( "/{id}" )
+    public ResponseEntity<?> findById( @PathVariable String id ) {
+        try {
+            return new ResponseEntity<>(userService.findById(id), HttpStatus.ACCEPTED);
+        } catch ( UserServiceException ex ) {
+            Logger.getLogger(UserController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<String>(ex.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create( @RequestBody UserDto userDto ) {
+        ModelMapper modelMapper = new ModelMapper();
+        User user = modelMapper.map(userDto, User.class);
+
+        userService.create(user);
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping( "/{id}" )
+    public ResponseEntity<?> update( @RequestBody UserDto user, @PathVariable String id ) {
+        ModelMapper modelMapper = new ModelMapper();
+        try {
+            User userMapped = modelMapper.map(user, User.class);
+            userService.update(userMapped, id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (UserServiceException ex) {
+            Logger.getLogger(UserController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.FORBIDDEN);
+        }
+    }
+
+    @DeleteMapping( "/{id}" )
+    public ResponseEntity<?> delete( @PathVariable String id ) {
+        try {
+            userService.deleteById(id);
+            return new ResponseEntity<>(true, HttpStatus.OK);
+        } catch (UserServiceException ex) {
+            Logger.getLogger(UserController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.FORBIDDEN);
+        }
+    }
+}
