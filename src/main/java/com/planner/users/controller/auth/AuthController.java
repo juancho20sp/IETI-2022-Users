711d2417e1762a0d21cbb6a2287554f4b9d73fbf
Juan David Murillo <juancho20sp@gmail.com>
2022-09-25 14:56:52 -0500

    Cross Origin added

--- src/main/java/com/planner/users/controller/auth/AuthController.java
+++ src/main/java/com/planner/users/controller/auth/AuthController.java
@@ -11,10 +11,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCrypt;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -22,6 +19,7 @@ import java.util.Date;
 import static com.planner.users.utils.constants.Constants.CLAIMS_ROLES_KEY;
 import static com.planner.users.utils.constants.Constants.TOKEN_DURATION_MINUTES;
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/v1/auth")
 public class AuthController {
